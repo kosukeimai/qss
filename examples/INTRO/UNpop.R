1725e36ecceffd1c2412f476375a58eb98746d29
Kosuke Imai <kimai@princeton.edu>
2015-07-18 17:39:56 -0400

    added example data sets for INTRO

---
+++ examples/INTRO/UNpop.R
@@ -0,0 +1,8 @@
+##
+## File: UNpop.R
+## Author: Kosuke Imai
+## The code loads the UN population data and saves it as a STATA file
+##
+library(foreign)
+UNpop <- read.csv("UNpop.csv")
+UNpop$world.pop <- UNpop$world.pop / 1000 # population in millions write.dta(UNpop, file = "UNpop.dta")
