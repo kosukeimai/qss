b47b6aec28a51756d2ca170cb5243fc8fc3b989e
winston-chou <emailwinston@gmail.com>
2015-07-19 22:26:03 -0400

    Update InstallForWindows.R

--- InstallForWindows.R
+++ InstallForWindows.R
@@ -3,4 +3,4 @@ install.packages("Rtools")
 install.packages("devtools")
 install.packages("swirl")
 devtools::install_github("dgrtwo/rparse") # install rparse
-devtools::install_github("dimagor/socraticswirl", ref = "fsi") # install SocraticSwril
+devtools::install_github("dimagor/socraticswirl", ref = "fsi") # install SocraticSwirl
