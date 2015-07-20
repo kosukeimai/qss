c87429c260936cdc3e20faedddc1d533fbbbeec6
winston-chou <emailwinston@gmail.com>
2015-07-20 08:23:19 -0400

    Delete InstallForWindows.R

--- InstallForWindows.R
+++
@@ -1,6 +0,0 @@
-## Windows installation
-install.packages("Rtools")
-install.packages("devtools")
-install.packages("swirl")
-devtools::install_github("dgrtwo/rparse") # install rparse
-devtools::install_github("dimagor/socraticswirl", ref = "fsi") # install SocraticSwirl
