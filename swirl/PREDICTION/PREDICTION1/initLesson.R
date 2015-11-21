31d2169e284c7ffcd7e4c5b48dc2e8812f5b8ffd
Kosuke Imai <kimai@princeton.edu>
2015-11-21 08:30:11 -0500

    moved the swirl files to qss-swirl

--- swirl/PREDICTION/PREDICTION1/initLesson.R
+++
@@ -1,7 +0,0 @@
-# Code placed in this file fill be executed every time the
-# lesson is started. Any variables created here will show up in
-# the user's working directory and thus be accessible to them
-# throughout the lesson.
-
-n <- 10
-fruit <- c("apple", "orange", "banana", "orange", "apple", "apple")
--- swirl/PREDICTION/PREDICTION2/initLesson.R
+++
@@ -1,9 +0,0 @@
-# Code placed in this file fill be executed every time the
-# lesson is started. Any variables created here will show up in
-# the user's working directory and thus be accessible to them
-# throughout the lesson.
-
-fruit <- c("apple", "orange", "banana", "orange", "apple", "apple")
-
-x <- sample(100)
-y <- x + rnorm(100, mean = 0, sd = 0.5)
